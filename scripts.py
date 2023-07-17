d7c6cc87980fdd204c82c0ba7831a804e33aeb46
DeFchanel <georgyasr@mail.ru>
2023-07-17 19:48:21 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,45 +1,35 @@
-def remove_chastisements(schoolkid_name):
+PRAISES = [
+    'Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!', 'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!', 'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший  ответ!', 'Талантливо!', 'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!'
+]
+
+def get_schoolkid(schoolkid_name):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains = schoolkid_name)
-    except ObjectDoesNotExist:
+    except Schoolkid.DoesNotExist:
         print("Извините, ученик не найден.")
         return
-    except MultipleObjectsReturned:
+    except Schoolkid.MultipleObjectsReturned:
         print("Извините, найдено несколько учеников.")
         return
+    return schoolkid
+
+
+def remove_chastisements(schoolkid_name):
+    schoolkid = get_schoolkid(schoolkid_name)
     remarks = Chastisement.objects.filter(schoolkid=schoolkid)
     remarks.delete()
 
 
 def fix_marks(schoolkid_name):
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains = schoolkid_name)
-    except ObjectDoesNotExist:
-        print("Извините, ученик не найден.")
-        return
-    except MultipleObjectsReturned:
-        print("Извините, найдено несколько учеников.")
-        return
-    marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
-    for mark in marks:
-        mark.points = 5
-        mark.save()
+    schoolkid = get_schoolkid(schoolkid_name)
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
+
 
 def create_commendation(schoolkid_name, subject):
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains = schoolkid_name)
-    except ObjectDoesNotExist:
-        print("Извините, ученик не найден.")
-        return
-    except MultipleObjectsReturned:
-        print("Извините, найдено несколько учеников.")
-        return
-    try:
-        lessons = Lesson.objects.filter(year_of_study=6, group_letter='А', subject__title=subject)
-        lesson = lessons[0]
-    except IndexError:
+    schoolkid = get_schoolkid(schoolkid_name)
+    lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject__title=subject).order_by('?').first()
+    if not lesson:
         print("Извините, предмет не найден. Проверьте вписанный вами предмет.")
         return
-    praises = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!', 'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!', 'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший  ответ!', 'Талантливо!', 'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!']
-    praise = random.choice(praises)
+    praise = random.choice(PRAISES)
     Commendation.objects.create(text = praise, created = lesson.date, schoolkid = schoolkid, subject = lesson.subject, teacher = lesson.teacher)
